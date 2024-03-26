7d7b6cc6a0d9be15ad5bc1a80eb9ed854eb9dfcf
mroreoman <123257969+mroreoman@users.noreply.github.com>
2024-03-26 17:27:07 -0400

    mostly finished during game prog class
    still has repeating letters bug

--- wordle.py
+++ wordle.py
@@ -1,9 +1,34 @@
 import random
 import tkinter as tk
-from enum import Enum
-from colorama import Fore
 
-Hints = Enum("Hints", ["GREY", "YELLOW", "GREEN"])
+alphabet = {
+    'b':None,
+    'c':None,
+    'a':None,
+    'd':None,
+    'e':None,
+    'f':None,
+    'g':None,
+    'h':None,
+    'i':None,
+    'j':None,
+    'k':None,
+    'l':None,
+    'm':None,
+    'n':None,
+    'o':None,
+    'p':None,
+    'q':None,
+    'r':None,
+    's':None,
+    't':None,
+    'u':None,
+    'v':None,
+    'w':None,
+    'x':None,
+    'y':None,
+    'z':None
+}
 
 wordleLa = []
 wordleTa = []
@@ -25,53 +50,79 @@ with open(r"wordleDictionary\wordle-Ta.txt", "r") as file:
 word = random.choice(wordleLa).strip()
 guesses = 0
 
-root = tk.Tk()
-root.geometry('600x400')
-
 def submit():
     global guesses
+    global alphabet
+
+    guess = guess_var.get()
+
+    if not guess.isalpha():
+        error.config(text="guess is not only letters")
+        return
+    elif len(guess) != 5:
+        error.config(text="guess is wrong length")
+        return
+    elif not (guess in wordleLa or guess in wordleTa):
+        error.config(text="guess is not in word list")
+        return
+    
+    error.config(text="")
 
     guesses += 1
-    guess = guess_var.get()
-    out = [["", None] for i in range(5)]
+
+    guess_frame = tk.Frame(root)
+    guess_frame.pack(side=tk.TOP)
+
+    color = ''
     for i, letter in enumerate(guess):
-        out[i][0] = letter
         if letter not in word:
-            out[i][1] = Hints.GREY
+            color = 'grey'
+            alphabet[letter].config(bg='grey')
         elif letter == word[i]:
-            out[i][1] = Hints.GREEN
+            color = 'green'
+            alphabet[letter].config(bg='green')
         elif guess.count(letter) > word.count(letter):
-            if guess[:i+1].count(letter) - sum([1 for a, b in zip(word[i:], guess[i:]) if a == b and a == letter]) > word.count(letter): #subtract number of green letter after i
-                out[i][1] = Hints.GREY
+            # TODO: something wrong!!!!
+            if guess[:i+1].count(letter) - sum([1 for a, b in zip(word[i:], guess[i:]) if a == b and a == letter]) > word.count(letter): #subtract number of green letters after i
+                color = 'grey'
+                if alphabet[letter].cget('bg') == 'white':
+                    alphabet[letter].config(bg='grey')
             else:
-                out[i][1] = Hints.YELLOW
+                color = 'yellow'
+                if alphabet[letter].cget('bg') == 'white':
+                    alphabet[letter].config(bg='yellow')
         else:
-            out[i][1] = Hints.YELLOW
+            color = 'yellow'
+            if alphabet[letter].cget('bg') == 'white':
+                alphabet[letter].config(bg='yellow')
+        
+        label = tk.Label(guess_frame, text = letter)
+        label.config(bg=color)
+        label.pack(side=tk.LEFT)
 
-def validate(input):
-    if not input.isalpha():
-        print("guess is not only letters")
-        return False
-    elif len(input) != 5:
-        print("guess is wrong length")
-        return False
-    elif not (input in wordleLa or input in wordleTa):
-        print("guess is not in word list")
-        return False
-    else:
-        return True
+root = tk.Tk()
+# root.geometry('600x400')
 
+keyboard_frames = [tk.Frame(root) for i in range(3)]
+keyboard = ['zxcvbnm', 'asdfghjkl', 'qwertyuiop']
+for i, row in enumerate(keyboard): # TODO: update this every guess
+    keyboard_frames[i].pack(side=tk.BOTTOM)
+    for letter in row:
+        label = tk.Label(keyboard_frames[i], text=letter, bg='white')
+        label.pack(side=tk.LEFT)
+        alphabet[letter] = label
+
+entry_frame = tk.Frame(root)
 guess_var = tk.StringVar()
+entry_label = tk.Label(entry_frame, text = 'guess:')
+entry = tk.Entry(entry_frame, textvariable=guess_var)
+btn = tk.Button(entry_frame, text='submit', command=submit)
+error = tk.Label(root)
 
-guess_label = tk.Label(root, text = 'guess:')
-
-entry = tk.Entry(root, textvariable=guess_var)
-reg = root.register(validate)
-entry.config(validate='key', validatecommand=(reg, '%P'))
-
-btn = tk.Button(root, text='submit', command=submit)
-guess_label.grid(row=0, column=0)
-entry.grid(row=0, column=1)
-btn.grid(row=1, column=1)
+entry_frame.pack(side=tk.BOTTOM)
+entry_label.pack(side=tk.LEFT)
+entry.pack(side=tk.LEFT)
+btn.pack(side=tk.LEFT)
+error.pack(side=tk.BOTTOM)
 
 root.mainloop()
